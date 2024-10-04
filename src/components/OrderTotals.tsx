63a3861d8c5abe3f5f07fd64c5dfe3a0bbe59bfe
DaniM0613 <wwh.rj.92@gmail.com>
2024-10-03 22:53:46 -0600

    Creando el botón para guardar la orden y crear la función de place order en nuestro hook de useOrder, importamos el placeOrder en app.tsx e importamos en orderTotal. Para reiniciar nuestra orden seteamos la order y la propina a sus valores iniciales, por ultimo realizamos la comprobación para que cuando empecemos agregar items a nuestro consumo se muestre nuestros componentes

--- src/components/OrderTotals.tsx
+++ src/components/OrderTotals.tsx
@@ -1,39 +1,44 @@
-import { useMemo } from "react"
+import { useCallback } from "react"
 import type { OrderItem } from "../types"
 import { formatCurrency } from "../helpers"
 
 type OrderTotalsProps = {
     order: OrderItem[],
-    tip: number
+    tip: number,
+    placeOrder: () => void
 
 }
 
-export default function OrderTotals({order, tip} : OrderTotalsProps) {
+export default function OrderTotals({order, tip, placeOrder} : OrderTotalsProps) {
 
-    const subtotalAmount = useMemo(() => order.reduce((total, item) => total + (item.quantity * item.price), 0 ) , [order])
-    const tipAmount = useMemo(() => subtotalAmount * tip, [tip, order]) 
-    const totalAmount = useMemo(() => subtotalAmount + tipAmount, [tip, order])
+    const subtotalAmount = useCallback(() => order.reduce((total, item) => total + (item.quantity * item.price), 0 ) , [order])
+    const tipAmount = useCallback(() => subtotalAmount() * tip, [tip, order]) 
+    const totalAmount = useCallback(() => subtotalAmount() + tipAmount(), [tip, order])
 
   return (
     <>
       <div className="space-y-3">
         <h2 className="font-black text-2xl">Totales y Propina: </h2>
         <p> Subtotal a Pagar: {''}
-            <span className="font-bold">{formatCurrency(subtotalAmount)}</span>
+            <span className="font-bold">{formatCurrency(subtotalAmount())}</span>
         </p>
 
         <p> Propina : {''}
-            <span className="font-bold">{formatCurrency(tipAmount)}</span>
+            <span className="font-bold">{formatCurrency(tipAmount())}</span>
         </p>
 
         <p> Total a Pagar: {''}
-            <span className="font-bold">{formatCurrency(totalAmount)}</span>
+            <span className="font-bold">{formatCurrency(totalAmount())}</span>
         </p>
 
       </div>
 
-      <button>
-
+      <button 
+         className="w-full p-3 bg-black uppercase text-white font-bold mt-10 disabled:opacity-10"
+         disabled={totalAmount() === 0}
+         onClick={placeOrder}
+      >
+        Guardar orden 
       </button>
     </>
   )
